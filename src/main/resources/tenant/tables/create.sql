34100044b7f14065940505608bfcf1c62e0809a0
AbineshKrishnan <abineshr@piraiinfo.com>
2024-10-07 10:36:27 +0530

    updated the sql queries

--- src/main/resources/tenant/tables/create.sql
+++ src/main/resources/tenant/tables/create.sql
@@ -1,15 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-CREATE TABLE IF NOT EXISTS sample_table (
+CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    tenant_id UUID,
-    otp INTEGER,
-    otp_expiry_time TIMESTAMP,
-    otp_type VARCHAR(20),
-    active BOOLEAN,
-    created_by VARCHAR(30),
-    updated_by VARCHAR(30),
+    user_name VARCHAR(255),
+    email_id VARCHAR(255),
+    created_by UUID,
+    updated_by UUID,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
     is_deleted BOOLEAN
